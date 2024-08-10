0137270350f4988571bf5af52d2524eb8281c25e
Ilya Andreev <bugdealer@icloud.com>
2024-08-10 20:52:58 +0300

    tmp v1.0

--- Formula/tmp.rb
+++ Formula/tmp.rb
@@ -1,17 +1,21 @@
 class Tmp < Formula
   desc "RAII-wrappers for unique temporary files and directories for modern C++"
   homepage "https://github.com/bugdea1er/tmp"
-  url "https://api.github.com/repos/bugdea1er/tmp/tarball/v0.9"
-  sha256 "589a991d8baa8567bfed3c5b7ea13931ad9b74d6c93fa5ff4eb9f827edfe9428"
+  url "https://github.com/bugdea1er/tmp/archive/refs/tags/v1.0.tar.gz"
+  sha256 "d00228f5913af02434cbc8d0a9e595dd9822d5038fe711c4df6c0c0870e5a773"
   license "MIT"
   head "https://github.com/bugdea1er/tmp.git", branch: "main"
 
   depends_on "cmake" => :build
 
   def install
-    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "-S", ".", "-B", "build", "-DBUILD_SHARED_LIBS=TRUE", *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
+    
+    system "cmake", "-S", ".", "-B", "build-static", "-DBUILD_SHARED_LIBS=FALSE", *std_cmake_args
+    system "cmake", "--build", "build-static"
+    lib.install "build-static/libtmp.a"
   end
 
   test do
