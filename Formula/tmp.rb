a64772871cbc51de59a7101dbee1651919c0481d
Ilya Andreev <bugdealer@icloud.com>
2024-09-22 01:45:56 +0300

    tmp v1.1 (#5)

--- Formula/tmp.rb
+++ Formula/tmp.rb
@@ -1,8 +1,8 @@
 class Tmp < Formula
   desc "RAII-wrappers for unique temporary files and directories for modern C++"
   homepage "https://github.com/bugdea1er/tmp"
-  url "https://github.com/bugdea1er/tmp/archive/refs/tags/v1.0.tar.gz"
-  sha256 "662e8dcf924b4838a1b28dce61d0d6631daee9a66cf26728017263df4011cd5e"
+  url "https://github.com/bugdea1er/tmp/archive/refs/tags/v1.1.tar.gz"
+  sha256 "e205682f3d124a2c5fe22f9fc0be2a714f87996970e2fc5a6fdaa05c7e9e8760"
   license "MIT"
   head "https://github.com/bugdea1er/tmp.git", branch: "main"
 
